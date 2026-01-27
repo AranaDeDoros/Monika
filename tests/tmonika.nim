117e816f43f7a58fd77b712a430575b401699c9d
AranaDeDoros <2879168-devfloresitver@users.noreply.gitlab.com>
2026-01-26 22:18:08 -0600

    tests updated

--- tests/tmonika.nim
+++ tests/tmonika.nim
@@ -5,7 +5,6 @@ import monika/halfwidthconverter
 suite "Monika Japanese String Utilities":
 
   setup:
-    # This runs before every test (like 'before' in Scala)
     let hira = "こんにちは"
     let kata = "ハロー"
 
@@ -18,5 +17,28 @@ suite "Monika Japanese String Utilities":
     check toHalfWidth(full) == "ﾊﾛｰ､ﾜｰﾙﾄﾞ!"
 
   test "monika wrapper logic":
-    # Testing your JpnStr converter
     check "漢字".hasKanji == true
+
+  test "contains only works correctly":
+    let str1 = "カタカナ"
+    let str2 = "カタカナ漢字"
+    check str1.containsOnly({Katakana}) == true
+    check str2.containsOnly({Katakana}) == false
+
+  test "hiragana to katakana":
+    let hiraStr = "あいうえお"
+    let kataStr = hiraStr.hiraToKata()
+    check kataStr == "アイウエオ"
+
+   test "katakana to hiragana":
+    let kataStr = "アイウエオ"
+    let hiraStr = kataStr.kataToHira()
+    check hiraStr == "あいうえお"
+
+  test "script summary works correctly":
+    let mixedStr = "あいうカナ漢字abc"
+    let summary = mixedStr.scriptSummary()
+    check summary.hiragana == 3
+    check summary.katakana == 2
+    check summary.kanji == 2
+    check summary.other == 3
